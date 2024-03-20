ae4379cd8612d50afb1c17f33505e5702626cd48
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-03-20 14:40:03 -0300

    Plotando gráfico de multiplas linhas com legenda

--- geragrafico.py
+++ geragrafico.py
@@ -7,8 +7,17 @@ def main():
     leitor = LeitorArquivo('data.txt')
     valores = leitor.getValores()
     print(valores)
+
+    plt.title('Gráfico de linhas')
+    plt.ylabel('Valores de entrada')
+    plt.xlabel('Amostragem')
     
-    plt.plot(valores)
+    i = 1
+    for serie in valores:
+        plt.plot(serie, label='Série ' + str(i))   
+        i += 1
+    plt.legend(loc='upper left')
+
     plt.show()
 
 
